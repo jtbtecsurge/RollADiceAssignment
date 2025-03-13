ca82b6adced65e7384fb0d1f97f0e04da885b2db
Jaimes Eduard T. Barlizo <jaimes.barlizo@tecsurge.com>
2025-03-13 15:34:45 +0800

    Initial Commit

---
+++ RollADiceAssignment/Program.cs
@@ -0,0 +1,4 @@
+﻿Random random = new Random();
+int randomNumber = random.Next(1, 7); // Generates a random number between 1 and 6
+Console.WriteLine("Random Number: " + randomNumber);
+Console.ReadKey();
